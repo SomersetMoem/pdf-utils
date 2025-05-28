c1a3012069e01122eb905d5ae7541e1f37c99308
SomersetMoem <nnekit10@mail.ru>
2025-05-28 10:08:48 +0300

    add file

--- src/test/java/com/example/PdfTest.java
+++ src/test/java/com/example/PdfTest.java
@@ -1,5 +1,25 @@
 package com.example;
 
-public class PdfTest {
+import com.pdf.PdfUtils;
+import com.pdf.extractors.PdfTextExtractor;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+public class PdfTest {
+    private PdfTextExtractor textExtractor;
+
+    @BeforeEach
+    public void setUp() {
+        PdfUtils pdfUtils = PdfUtils.fromPath("src/test/resources/test.data/testPdf.pdf");
+        textExtractor = new PdfTextExtractor(pdfUtils.getDocument());
+    }
+
+
+    @Test
+    public void getAllText_shouldContainTextFromAllPages() {
+        String allText = textExtractor.getAllText();
+        Assertions.assertTrue(allText.contains("Этапы. Угрозы. Стратегии"));
+        Assertions.assertTrue(allText.contains("Арт-директор Алексей Богомолов"));
+    }
 }
